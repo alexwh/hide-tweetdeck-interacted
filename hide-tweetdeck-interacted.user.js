9b11a387c64d7302ca4bde6ffb4d1207e7a79f4b
alexwh <alexwh@users.noreply.github.com>
2020-06-24 23:02:21 +0100

    observe app container to unhide before setting up

--- hide-tweetdeck-interacted.user.js
+++ hide-tweetdeck-interacted.user.js
@@ -2,34 +2,45 @@
 // @name    hide already liked/retweeted tweetdeck
 // @version 1.0
 // @match   https://tweetdeck.twitter.com/*
-// @run-at  document-idle
 // ==/UserScript==
 
 (function() {
   'use strict';
 
-  const observer = new MutationObserver(mutations => {
+  const opts = {childList: true, subtree: true};
+
+  const appObserver = new MutationObserver(mutations => {
     mutations.forEach((mutation) => {
-      if (mutation.type === 'childList' &&
-          mutation.target.className.includes("chirp-container") &&
-          mutation.addedNodes.length > 1) {
+      if (mutation.oldValue.includes("is-hidden") && !mutation.target.className.includes("is-hidden")) {
+        for (let column of document.querySelectorAll(`section.column`)) {
+          // don't hide stuff in the notifications column
+          if (column.querySelectorAll(`i.icon-notifications`).length < 1) {
+            //console.log("observing", column);
+            tweetObserver.observe(column, opts);
+          }
+        }
+        // for e.g. user tweet modals containing chirp-containers
+        tweetObserver.observe(document.querySelector(`div#open-modal`), opts);
+        // don't set up more than once
+        appObserver.disconnect();
+      }
+    });
+  });
+
+  const tweetObserver = new MutationObserver(mutations => {
+    mutations.forEach((mutation) => {
+      if (mutation.target.className.includes("chirp-container") && mutation.addedNodes.length > 1) {
         for (let tweet of mutation.addedNodes) {
+          // liked or retweeted
           if (tweet.querySelectorAll(`i.js-icon-favorite.icon-heart-filled, i.js-icon-retweet.icon-retweet-filled`).length > 0) {
             tweet.style.display = "none";
-            console.log("rm", tweet);
+            //console.log("rm", tweet);
           }
         }
       }
     });
   });
 
-  const opts = {childList: true, subtree: true};
-  for (let column of document.querySelectorAll(`section.column`)) {
-    if (column.querySelectorAll(`i.icon-notifications`).length < 1) {
-      console.log("observing", column);
-      //observer.observe(column, opts);
-    }
-  }
-  // for e.g. user tweet modals containing chirp-containers
-  observer.observe(document.querySelector(`div#open-modal`), opts);
+  // wait for app (and columns) to load before observing them
+  appObserver.observe(document.querySelector(`div.application`), {attributeFilter: ["class"], attributeOldValue: true});
 })();
